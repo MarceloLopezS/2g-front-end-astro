ff8ad221508ccc9a9224840a83db4e4241961ad9
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-12-20 20:18:49 -0500

    Replace Grid with Carousel
    On ProjectsGrid component.
    Update property names and selectors accordingly.

--- src/widgets/ProjectsSection/config/DOMSelectors.js
+++ src/widgets/ProjectsSection/config/DOMSelectors.js
@@ -1,3 +1,3 @@
-export const PROJECTS_GRID = "section.projects__grid";
+export const PROJECTS_WRAPPER = "section.projects__wrapper";
 export const PROJECT_CARDS = ".card[data-section='projects']";
 export const PROJECT_CARD_BODY = ".card[data-section='projects'] .card__body";
