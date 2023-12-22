c6d6d440d343355f1869dca4e67b50b7b0bd1afb
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-12-22 13:03:19 -0500

    Improve selectors specificity for JS

--- src/widgets/AboutSection/config/DOMSelectors.js
+++ src/widgets/AboutSection/config/DOMSelectors.js
@@ -1,2 +1,3 @@
 export const DESCRIPTION_SECTION = ".about-us__description";
 export const DESCRIPTION_SLIDE = ".text-slide";
+export const DESCRIPTION_EXTRA_INFO_BUTTON = ".about-us__extra-info-button--toggler";
