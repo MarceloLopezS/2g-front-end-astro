ecf3a7642a69adb31a5445ae33b4e4a682506d1b
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2023-12-26 14:05:19 -0500

    Add github action for deployment to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://MarceloLopezS.github.io',
+  base: '/2g-front-end-astro',
+});
