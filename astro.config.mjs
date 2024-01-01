f009d19c51fec1b4e8f28a913ebe4fc51b1f3b75
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-01-01 16:45:20 -0500

    Remove config for gh pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://MarceloLopezS.github.io',
-  base: '/2g-front-end-astro',
+  site: 'https://2gserviciosgenerales.com/'
 });
