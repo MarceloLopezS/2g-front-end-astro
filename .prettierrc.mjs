5b7e6656e63951633b2487893510404eef941cf5
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-12-27 15:12:13 -0500

    update(prettier): formatting config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro"],
+  arrowParens: "avoid",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
   overrides: [
     {
       files: "*.astro",
       options: {
-        parser: "astro",
-      },
-    },
-  ],
-};
+        parser: "astro"
+      }
+    }
+  ]
+}
