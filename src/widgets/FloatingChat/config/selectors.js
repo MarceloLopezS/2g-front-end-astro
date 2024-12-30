577f4168c6b19dbcb8cf2c6ac415ade7b661c770
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-12-30 14:24:21 -0500

    update(FloatingChat): handle dialog on message submit

--- src/widgets/FloatingChat/config/selectors.js
+++ src/widgets/FloatingChat/config/selectors.js
@@ -1,3 +1,5 @@
 export const TOGGLER_BTN = ".floating-chat__toggler"
 export const DIALOG = ".floating-chat__dialog"
 export const CLOSE_BTN = ".floating-chat__close-btn"
+export const USER_MESSAGE_FORM = ".floating-chat__user-message"
+export const USER_MESSAGE_INPUT = ".floating-chat__user-message textarea"
--- src/widgets/FloatingChat/model/handleFloatingChat.js
+++ src/widgets/FloatingChat/model/handleFloatingChat.js
@@ -18,11 +18,14 @@ const closeDialog = (dialog) => {
 const handleFloatingChat = ({
   togglerBtnSelector,
   dialogSelector,
-  closeBtnSelector
+  closeBtnSelector,
+  messageFormSelector,
+  messageInputSelector
 }) => {
   const dom_toggler = getDomElement(togglerBtnSelector)
   const dom_dialog = getDomElement(dialogSelector)
   const dom_closeButton = getDomElement(closeBtnSelector)
+  const dom_messageForm = getDomElement(messageFormSelector)
 
   const closeDialogOnEscKey = (e) => {
     if (!dom_dialog.hasAttribute("open")) return
@@ -33,15 +36,28 @@ const handleFloatingChat = ({
     }
   }
 
+  const handleMessageSubmit = (e) => {
+    const dom_messageInput =
+      getDomElement(messageInputSelector, dom_messageForm)
+
+    if (dom_messageInput.value === "") return e.preventDefault()
+
+    closeDialog(dom_dialog)()
+    dom_messageForm.reset()
+  }
+
   dom_toggler.addEventListener("click", () => {
     if (dom_dialog.hasAttribute("open")) {
       dom_dialog.removeEventListener("keydown", closeDialogOnEscKey)
+      dom_messageForm.removeEventListener("submit", handleMessageSubmit)
+
       closeDialog(dom_dialog)()
       return
     }
 
     dom_dialog.show()
     dom_dialog.addEventListener("keydown", closeDialogOnEscKey)
+    dom_messageForm.addEventListener("submit", handleMessageSubmit)
   })
 
   dom_closeButton.addEventListener("click", closeDialog(dom_dialog))
